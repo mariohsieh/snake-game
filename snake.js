d484a3b389ed5eeb7d87f77255b84bf421f31d9b
Mario Hsieh <mariohsieh@gmail.com>
2014-02-27 22:08:14 -0800

    lesson 5

--- snake.js
+++ snake.js
@@ -9,7 +9,7 @@ var drawSnake = function (snakeToDraw) {
 var snake = [{ top: 0, left: 0}];	//	create an array variable snake with starting coordinates
 drawSnake(snake);	// call drawSnake function;
 
-//
+// function to add 1 pixel to the top border and returns the new coordinates
 var moveSnake = function (snake) {
 	var oldSegment = snake[0];
 	var newSegment = { top: oldSegment.top + 1, left: oldSegment.left };	// adds 1 to the top margin
@@ -17,25 +17,21 @@ var moveSnake = function (snake) {
 	return newSnake;	// returns snake with new coordinates
 }
 
-snake = moveSnake(snake);	// each time you call moveSnake(), the snake moves down 1 pixel
-snake = moveSnake(snake);
-snake = moveSnake(snake);
-snake = moveSnake(snake);
-drawSnake(snake);
+// function to move the snake by updating its coordinates and redrawing the snake
+var advanceGame = function() {
+	snake = moveSnake(snake);
+	drawSnake(snake);	
+}
+
+//	executes the function advanceGame every 1 second. 
+CHUNK.executeNTimesPerSecond(advanceGame, 1);
 
 
-/*
-Why does adding 1 to a segments top value move it down?
-	top refers to the top margin
+// if done correctly, snake will travel down automatically
 
-Can you make the snake move right intead of down?
-	add "+ 1" to "oldSegment.left" instead
-
-What happens to the newSnake variable? Can you use it outside of the moveSnake function body (within the curly braces)?
-	newSnake variable gets returned by the function moveSnake.
-	it cannot be used outside of function body because it is a private variable.
-	
-Why are we re-assigning snake the results of moveSnake?
-	since calling moveSnake each time moves the snake down 1 pixel, re-assigning snake the results will move snake down 1 pixel as well
+/* Q & A's
+How can you increase the speed at which the snake moves?
+	2 ways:
+		-increase the redraw speed of CHUNK.executeNTimesPerSecond
+		-increase the movement of moveSnake.  (i.e: '+2' or '+3')
 */
-
