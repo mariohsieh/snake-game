f43095e0745e3e193355afd0a846d6f43b379f07
Mario Hsieh <mariohsieh@gmail.com>
2014-02-27 22:28:14 -0800

    lesson 6

--- snake.js
+++ snake.js
@@ -6,16 +6,57 @@ var drawSnake = function (snakeToDraw) {
 	CHUNK.draw(drawableObjects);	// call draw function
 }
 
-var snake = [{ top: 0, left: 0}];	//	create an array variable snake with starting coordinates
-drawSnake(snake);	// call drawSnake function;
+// function to move in 4 directions.  a series of 'if else' statements to determine what direction to move
+var moveSegment = function(segment) {
 
-// function to add 1 pixel to the top border and returns the new coordinates
+	if (segment.direction == "down") {
+		return { top: segment.top + 1, left: segment.left }
+	} else if (segment.direction == "up") {
+		return { top: segment.top - 1, left: segment.left }
+	} else if (segment.direction == "right") {
+		return { top: segment.top, left: segment.left + 1 }
+	} else if (segment.direction == "left") {
+		return { top: segment.top, left: segment.left - 1}
+	}
+
+/*
+	// moves the snake by adjusting the margins 'left' or 'top'
+	switch(segment.direction) {
+		case "down":
+			//return { top: segment.top + 1, left: segment.left }
+			segment.top++;
+			break;
+		case "up":
+			//return { top: segment.top - 1, left: segment.left }
+			segment.top--;
+			break;
+		case "right":
+			//return { top: segment.top, left: segment.left + 1 }
+			segment.left++;
+			break;
+		case "left":
+			//return { top: segment.top, left: segment.left - 1 }
+			segment.left--;
+			break;
+		default:
+	}
+*/
+
+	return segment;
+
+}
+
+// function to move snake 1 pixel in the direction determined by moveSegment function
 var moveSnake = function (snake) {
 	var oldSegment = snake[0];
-	var newSegment = { top: oldSegment.top + 1, left: oldSegment.left };	// adds 1 to the top margin
-	var newSnake = [newSegment]; // puts snake with new coordinates into an array
+	var newSegment = moveSegment(oldSegment);	//	get new coordinates from moveSegment function
+	newSegment.direction = oldSegment.direction;	// keeps the same direction
+	var newSnake = [newSegment];	// puts new coordinates into newSnake array variable
 	return newSnake;	// returns snake with new coordinates
 }
+
+//	create an array variable snake with starting coordinates and direction of movement
+var snake = [{ top: 0, left: 0, direction: "down"}];	
 
 // function to move the snake by updating its coordinates and redrawing the snake
 var advanceGame = function() {
@@ -27,11 +68,37 @@ var advanceGame = function() {
 CHUNK.executeNTimesPerSecond(advanceGame, 1);
 
 
-// if done correctly, snake will travel down automatically
-
 /* Q & A's
-How can you increase the speed at which the snake moves?
-	2 ways:
-		-increase the redraw speed of CHUNK.executeNTimesPerSecond
-		-increase the movement of moveSnake.  (i.e: '+2' or '+3')
+Use a switch statement instead of 'if else'
+	
+	switch(segment.direction) {
+		case "down":
+			return { top: segment.top + 1, left: segment.left }
+			break;
+		case "up":
+			return { top: segment.top - 1, left: segment.left }
+			break;
+		case "right":
+			return { top: segment.top, left: segment.left + 1 }
+			break;
+		case "left":
+			return { top: segment.top, left: segment.left - 1 }
+			break;
+		default:
+	}
+
+Change the starting snake's segments to ensure all the directions work
+	var snake = [{ top: 10, left: 10, direction: "left"}];
 */
+
+
+
+
+
+
+
+
+
+
+
+
